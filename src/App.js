1ded51c20623372cb169ad9beaa7fc7a9dd97ed1
fs-projects <42044009+fs-projects@users.noreply.github.com>
2022-11-25 18:13:22 +0530

    Add and Update: Introduce label tag instead of expanding input tag over the button. Accessibility comprised though.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ImUpload } from 'react-icons/im';
 import './style.scss';
 
 export default function App() {
+  const [file, setFile] = useState(null);
+  const handleUpload = (e) => {
+    console.log('event', e.target.files[0]);
+  };
   return (
     <div class="container">
-      <button class="upload"><span class="btnTitle">Upload</span></button>
+      <input
+        type="file"
+        accept=".csv"
+        id="csvFile"
+        className="fileInput"
+        onChange={(e) => handleUpload(e)}
+      />
+      <label for="csvFile">
+        <div className="upload">
+          <div className="uploadContent">
+            <ImUpload className="uploadIcon" />
+            <span className="btnTitle">Upload</span>
+          </div>
+        </div>
+      </label>
     </div>
   );
 }
