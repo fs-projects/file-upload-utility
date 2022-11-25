31bc45ee1dc8ea0e71fc07ea8a6833393101efd3
fs-projects <42044009+fs-projects@users.noreply.github.com>
2022-11-25 13:57:29 +0530

    Add: Initial code for landing page and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.scss';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div class="container">
+      <button class="upload"><span class="btnTitle">Upload</span></button>
     </div>
   );
 }
